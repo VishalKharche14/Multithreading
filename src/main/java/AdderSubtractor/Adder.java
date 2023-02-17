d890640f90279f74d60333c9be5712fe47f80569
Vishal <mh20bu3372@gmail.com>
2023-02-17 23:47:21 +0530

    Shared Count - Adder Subtractor MultiThreading

---
+++ src/main/java/AdderSubtractor/Adder.java
@@ -0,0 +1,16 @@
+package AdderSubtractor;
+
+public class Adder implements Runnable{
+    private Count count;
+
+    public Adder(Count count) {
+        this.count = count;
+    }
+
+    @Override
+    public void run() {
+        for(int i=1; i<1001; i++){
+            count.addValue(i);
+        }
+    }
+}
---
+++ src/main/java/AdderSubtractor/Subtractor.java
@@ -0,0 +1,16 @@
+package AdderSubtractor;
+
+public class Subtractor implements Runnable {
+    private Count count;
+
+    public Subtractor(Count count) {
+        this.count = count;
+    }
+
+    @Override
+    public void run() {
+        for(int i=1; i<1001; i++){
+            count.addValue(-i);
+        }
+    }
+}
